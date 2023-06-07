6d604804f290dd82171147eee4d2a2953c19c8bd
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-06-07 18:57:56 +0200

    feat: Create database seed for quotes

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Quote;
+
+class QuoteController extends Controller
+{
+    public function index()
+    {
+        return view('index', [
+            'quotes' => Quote::all()
+        ]);
+    }
+}
