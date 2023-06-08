c538797a7e9aac3d1ba89185f1125dfd46eec80b
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-06-08 20:00:47 +0200

    feat: Show message when there is no any quote in the database

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,16 @@
 <x-layout>
     <div class="container d-flex mt-5 flex-wrap justify-content-around">
-        @foreach($quotes as $quote)
-            <div class="d-flex main-page-quote mb-3 bg-light p-4 rounded">
-                <a href="/quote/{{ $quote->id }}">{{ $quote->body }}</a>
+        @if (count($quotes) == 0)
+            <div class="d-flex main-page-quote mb-3 alert alert-primary p-4 rounded justify-content-center">
+                <p class="mb-0">No quotes at this time.</p>
             </div>
-        @endforeach
+        @else
+            @foreach($quotes as $quote)
+                <div class="d-flex main-page-quote mb-3 bg-light p-4 rounded">
+                    <a href="/quote/{{ $quote->id }}">{{ $quote->body }}</a>
+                </div>
+            @endforeach
+        @endif
     </div>
     <div class="container mt-5 mb-5">
         {{ $quotes->links() }}
