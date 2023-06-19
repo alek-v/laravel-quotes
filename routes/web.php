bc54291320c482f197baa5054f094d6009af90b2
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-06-19 15:08:39 +0200

    fix: Update name of the route

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use App\Http\Controllers\QuoteController;
 Route::get('/', [QuoteController::class, 'index'])->name('home');
 Route::get('quote/{quote:id}', [QuoteController::class, 'show']);
 Route::get('quotes/inspirational-quotes', function() { return view('quotes.inspirational-quotes'); })->name('inspirational-quotes');
-Route::get('quotes/vavok-quotes', function() { return view('quotes.vavok-quotes'); });
-Route::get('quotes/vavok-quotes/get', function() { return file_get_contents('https://api.vavok.net/quotes/random'); })->name('vavok-quotes');
+Route::get('quotes/vavok-quotes', function() { return view('quotes.vavok-quotes'); })->name('vavok-quotes');
+Route::get('quotes/vavok-quotes/get', function() { return file_get_contents('https://api.vavok.net/quotes/random'); });
